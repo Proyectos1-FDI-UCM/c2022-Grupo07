2f63b16ae7641c8e3e7631d7743dee693711ad50
Rafaa0514 <99506556+Rafaa0514@users.noreply.github.com>
2022-02-28 12:19:07 +0100

    Disparo Grav Hecho

--- Go Bug It!/Assets/Scripts/GravBulletComponent.cs
+++ Go Bug It!/Assets/Scripts/GravBulletComponent.cs
@@ -2,14 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GravitationalShootComponent : MonoBehaviour
+public class GravBulletComponent : MonoBehaviour
 {
     #region references
     private Collider2D _myCollider;
     #endregion
-    
+
     #region methods
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         Destroy(gameObject);
     }
