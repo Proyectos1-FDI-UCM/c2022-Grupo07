989a64c39e3367031aeb823479ed7b70e7dce89e
Zorromatamoscas <99506562+Zorromatamoscas@users.noreply.github.com>
2022-03-01 13:24:09 +0100

    windows hecho

---
+++ Go Bug It!/Assets/Scripts/Enemy_lifeComponent.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_lifeComponent : MonoBehaviour
+{
+    #region methods
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.layer==7)// Si el enemigo toca una deathzone, muere, se destruye.
+        {
+            Destroy(this.gameObject);
+            //Habrá que llamar al game manager para contabilizar los puntos
+        }
+    }
+    #endregion
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
