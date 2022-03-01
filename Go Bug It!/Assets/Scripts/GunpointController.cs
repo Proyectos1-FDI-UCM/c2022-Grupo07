f2f461862e9ed0a8dcfe30f70960cbd0dc92299d
Zorromatamoscas <99506562+Zorromatamoscas@users.noreply.github.com>
2022-03-01 21:55:17 +0100

    Offset de bala

--- Go Bug It!/Assets/Scripts/GunpointController.cs
+++ Go Bug It!/Assets/Scripts/GunpointController.cs
@@ -10,19 +10,35 @@ public class GunpointController : MonoBehaviour
     [SerializeField] private GameObject _gravShot; // Bala gravitatoria
     [SerializeField] private GameObject _neuShot; // Bala neutralizadora
     [SerializeField] private Transform Gun; // Posición del label Gun
+    InputController _myinput;
+    private Transform _playerTransform;
     #endregion
 
     #region properties
     public enum ShootType {Gravity, Neutralize}
     [SerializeField, Range(0, 1)] ShootType _shot = 0;
+    Vector2 _direction;
+    #endregion
+    #region parameters
+    [SerializeField]
+    private float _offset;
     #endregion
 
     #region methods
     // Instanciación de la bala
     public void Shoot()
     {
-        if (_shot == ShootType.Gravity) GameObject.Instantiate(_gravShot, _myTransform.position, _myTransform.rotation);
-        else GameObject.Instantiate(_neuShot, _myTransform.position, _myTransform.rotation);
+        bool _prevdirection=true;
+         if (_myinput.GetDirection() > 0) _prevdirection=true;// decide cual es la dirección de disparo según la dirección anterior.
+        else if (_myinput.GetDirection() < 0)_prevdirection= false;
+        if (_prevdirection == false)
+        {
+            _direction = _myTransform.position;
+            _direction.x -= (2 * _offset); 
+        }
+        else if (_prevdirection == true) _direction = _myTransform.position;// si es positivo, se dispara desde la posición del gunpoint (a la dcha.)
+        if (_shot == ShootType.Gravity) GameObject.Instantiate(_gravShot, _direction, _myTransform.rotation);
+        else GameObject.Instantiate(_neuShot, _direction, _myTransform.rotation);
     }
 
     // Cambio de disparo
@@ -47,5 +63,7 @@ public class GunpointController : MonoBehaviour
     void Start()
     {
         _myTransform = transform;
+        _playerTransform = GetComponentInParent<Transform>();
+        _myinput = GetComponentInParent<InputController>();
     }
 }
