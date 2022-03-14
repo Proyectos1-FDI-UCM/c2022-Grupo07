7a69a8039575b41c57da2c8c358f883ef6d95cce
Pavipollo17 <pagonz30@ucm.es>
2022-03-14 23:54:37 +0100

    Pantalla de controles, pausa y demás
    Varios cambios y avances:
    - Pantalla de controles implementada prácticamente en su totalidad.
    - Reorganización de estructura de archivos yde los prefabs.
    - Pulido y adición de sprites del/en el prefab de pausa: botones, animaciones, títulos, esquema de controles...
    - Implementación por código del comportamiento del menú de pausa.

--- Go Bug It!/Assets/Scripts/UI/PauseMenu.cs
+++ Go Bug It!/Assets/Scripts/UI/PauseMenu.cs
@@ -7,7 +7,11 @@ using UnityEngine.EventSystems;
 public class PauseMenu : MonoBehaviour
 {
     #region references
+    // Botones EventSystem
     [SerializeField] private GameObject _firstPauseButton;
+    // Pausa
+    [SerializeField] private GameObject _pauseUI, _controllsUI, _optionsUI;
+    private Transform _controllsTransform, _optionsTransform;
     #endregion
 
     #region methods
@@ -20,19 +24,42 @@ public class PauseMenu : MonoBehaviour
     // Mostrar controles
     public void ControllsMenu()
     {
-
+        _pauseUI.SetActive(false);
+        _controllsUI.SetActive(true);
+        _controllsTransform.localPosition = new Vector3(0, 0, 0);
     }
 
     // Mostrar las opciones
     public void OptionsMenu()
     {
+        _pauseUI.SetActive(false);
+        _optionsUI.SetActive(true);
+        _optionsTransform.localPosition = new Vector3(0, 0, 0);
+    }
 
+    public void Back(string option)
+    {
+        if (option == "Options")
+        {
+            _optionsUI.SetActive(false);
+            _optionsTransform.localPosition = new Vector3(-1920, 0, 0);
+        }
+        else
+        {
+            _controllsUI.SetActive(false);
+            _controllsTransform.localPosition = new Vector3(1920, 0, 0);
+        }
+
+        _pauseUI.SetActive(true);
     }
     #endregion
 
     // Start is called before the first frame update
     void Start()
     {
+        _optionsTransform = _optionsUI.transform;
+        _controllsTransform = _controllsUI.transform;
+
         // Asegurarse de que no se selecciona ningún botón de base
         EventSystem.current.SetSelectedGameObject(null);
 
