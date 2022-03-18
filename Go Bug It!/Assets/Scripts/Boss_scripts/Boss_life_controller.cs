a32d93b82c163bedcc42ac108d2f3be79ba6717a
Zorromatamoscas <99506562+Zorromatamoscas@users.noreply.github.com>
2022-03-18 11:41:11 +0100

    boss ini

---
+++ Go Bug It!/Assets/Scripts/Boss_scripts/Boss_life_controller.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss_life_controller : MonoBehaviour
+{
+    #region parameters
+    [SerializeField] private int life_points;
+    #endregion
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
