f5179a832efe27e5e2c9aaf9e87d8c35d6f79693
Zorromatamoscas <99506562+Zorromatamoscas@users.noreply.github.com>
2022-03-18 11:47:42 +0100

    boss ini 2

--- Go Bug It!/Assets/Scripts/Boss_scripts/Boss_life_controller.cs
+++ Go Bug It!/Assets/Scripts/Boss_scripts/Boss_life_controller.cs
@@ -7,6 +7,16 @@ public class Boss_life_controller : MonoBehaviour
     #region parameters
     [SerializeField] private int life_points;
     #endregion
+    #region
+    private int current_life;
+    #endregion
+    #region methods
+    public void Die()
+    {
+        if()
+    }
+
+    #endregion
     // Start is called before the first frame update
     void Start()
     {
