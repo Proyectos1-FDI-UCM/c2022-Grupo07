9f95b012eadccfbe919e4c2323a8c2e400d549d9
Zorromatamoscas <99506562+Zorromatamoscas@users.noreply.github.com>
2022-03-21 19:15:40 +0100

    Primera implementación de ataques

--- Go Bug It!/Assets/Scripts/Boss_scripts/Boss_attack_controller.cs
+++ Go Bug It!/Assets/Scripts/Boss_scripts/Boss_attack_controller.cs
@@ -4,11 +4,48 @@ using UnityEngine;
 
 public class Boss_attack_controller : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    #region references
+    [SerializeField] GameObject [] Avast;
+    [SerializeField] GameObject [] Norton;
+    [SerializeField] GameObject [] WDefender;
+    [SerializeField] GameObject[] McAfee;
+    #endregion
+    #region methods
+    IEnumerator DestroyAttack(float attackDuration, GameObject attackToDestroy)
     {
-        
+        yield return new WaitForSeconds(attackDuration);
+        Destroy(attackToDestroy);
+
     }
+    public void AvastAttack()
+    {
+        GameObject currentAvastattack= Instantiate(Avast[Random.Range(0, Avast.GetLength(0) - 1)]);
+        StartCoroutine(DestroyAttack(attackDuration, currentAvastattack));
+    }
+    public void NortonAttack()
+    {
+        GameObject currentNortonattack = Instantiate(Norton[Random.Range(0, Norton.GetLength(0) - 1)]);
+        StartCoroutine(DestroyAttack(attackDuration, currentNortonattack));
+    }
+    public void WDefenderAttack()
+    {
+        GameObject currentDefenderattack = Instantiate(WDefender[Random.Range(0, WDefender.GetLength(0) - 1)]);
+        StartCoroutine(DestroyAttack(attackDuration, currentDefenderattack));
+    }
+    public void McAfeeAttack()
+    {
+        GameObject currentMcAfeeattack = Instantiate(McAfee[Random.Range(0, McAfee.GetLength(0) - 1)]);
+        StartCoroutine(DestroyAttack(attackDuration, currentMcAfeeattack));
+    }
+
+    #endregion
+    #region parameters
+    [SerializeField] private float attackDuration;
+    [SerializeField] private float timeToAttack;
+    #endregion
+    #region properties
+
+    #endregion
 
     // Update is called once per frame
     void Update()
