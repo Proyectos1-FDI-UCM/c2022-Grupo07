36537a7157c8c4f20ed5aed52f64587a3faea74a
Pavipollo17 <pagonz30@ucm.es>
2022-04-03 14:33:32 +0200

    Sliders de SFX y General
    Añadidos al menú de pausa los sliders de SFX y General, editando el AudioMixer y su código.

--- Go Bug It!/Assets/Scripts/UI/VolumeManager.cs
+++ Go Bug It!/Assets/Scripts/UI/VolumeManager.cs
@@ -11,10 +11,22 @@ public class VolumeManager : MonoBehaviour
     #endregion
 
     #region methods
+    // Actualiza el valor del volumen general
+    public void SetGeneralVolumeLevel(float sliderValue)
+    {
+        _mixer.SetFloat("GeneralVolume", Mathf.Log10(sliderValue) * 20);
+    }
+
     // Actualiza el valor del volumen de la musica
-    public void SetVolumeLevel(float sliderValue)
+    public void SetMusicVolumeLevel(float sliderValue)
     {
         _mixer.SetFloat("MusicVolume", Mathf.Log10(sliderValue) * 20);
+    }
+
+    // Actualiza el valor del volumen del sonido
+    public void SetSoundVolumeLevel(float sliderValue)
+    {
+        _mixer.SetFloat("SFXVolume", Mathf.Log10(sliderValue) * 20);
     }
     #endregion
 
